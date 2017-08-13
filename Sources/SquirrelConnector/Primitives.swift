8dfb27104ea7b292f1be513c3878241b23e03cda
Filip Klembara <filip@klembara.pro>
2017-08-13 22:30:02 +0200

    add datettime

--- Sources/SquirrelConnector/Primitives.swift
+++ Sources/SquirrelConnector/Primitives.swift
@@ -6,6 +6,8 @@
 //
 //
 
+import Foundation
+
 public protocol Primitive {
     var mysqlType: String { get }
 }
@@ -14,70 +16,88 @@ extension UInt8: Primitive {
     public var mysqlType: String {
         return "TINYINT UNSIGNED"
     }
- }
+}
+
 extension UInt16: Primitive {
     public var mysqlType: String {
         return "SMALLINT UNSIGNED"
     }
- }
+}
+
 extension UInt32: Primitive {
     public var mysqlType: String {
         return "INT UNSIGNED"
     }
- }
+}
+
 extension UInt64: Primitive {
     public var mysqlType: String {
         return "BIGINT UNSIGNED"
     }
 }
+
 extension UInt: Primitive {
     public var mysqlType: String {
         return "BIGINT UNSIGNED"
     }
 }
+
 extension Int8: Primitive {
     public var mysqlType: String {
         return "TINYINT"
     }
 }
+
 extension Int16: Primitive {
     public var mysqlType: String {
         return "SMALLINT"
     }
 }
+
 extension Int32: Primitive {
     public var mysqlType: String {
         return "INT"
     }
 }
+
 extension Int64: Primitive {
     public var mysqlType: String {
         return "BIGINT"
     }
 }
+
 extension Int: Primitive {
     public var mysqlType: String {
         return "BIGINT"
     }
 }
+
 extension Double: Primitive {
     public var mysqlType: String {
         return "DOUBLE(16)"
     }
- }
+}
+
 extension Float: Primitive {
     public var mysqlType: String {
         return "FLOAT(6)"
     }
- }
+}
+
 extension Bool: Primitive {
     public var mysqlType: String {
         return "BOOL"
     }
- }
+}
+
 extension String: Primitive {
     public var mysqlType: String {
         return "TEXT"
     }
- }
+}
 
+extension Date: Primitive {
+    public var mysqlType: String {
+        return "DATETTIME"
+    }
+}
