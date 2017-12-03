832568ff02ade04c48bc491274c19a3b97e6440a
Filip Klembara <filip@klembara.pro>
2017-12-03 20:05:17 +0100

    remove cache

--- Sources/SquirrelConnector/Projectable.swift
+++ Sources/SquirrelConnector/Projectable.swift
@@ -11,21 +11,26 @@ import MongoKitten
 public protocol Projectable: Codable {
     init()
 }
-
+extension Projection {
+    init(attributes: [String]) {
+        let document = Document(dictionaryElements: attributes.map({ ($0, 1) }))
+        self.init(document)
+    }
+}
 extension Projectable {
     static var projection: Projection {
-        let cache = SquirrelConnectorCache.cache
-        let name = String(describing: type(of: self))
-
-        if let projection = cache.object(forKey: name) {
-            return projection
-        }
+//        let cache = SquirrelConnectorCache.cache
+//        let name = String(describing: type(of: self))
+//
+//        if let projection = cache.object(forKey: name) {
+//            return projection
+//        }
 
         let mirror = Mirror(reflecting: Self.init())
         let children = mirror.children
         let attributes = children.flatMap({ $0.label })
         let proj = Projection(attributes: attributes)
-        cache.async.addObject(proj, forKey: name)
+//        cache.async.addObject(proj, forKey: name)
         return proj
     }
 }
